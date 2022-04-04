7d440fa41ee245ddde8767750ee86777a9c90607
alisher <alisherkabildjanov@yahoo.com>
2022-04-04 10:53:28 -0500

    config changes

--- editor/packages/cli/src/commands/serve.ts
+++ editor/packages/cli/src/commands/serve.ts
@@ -1,6 +1,6 @@
+import path from 'path';
 import { Command } from 'commander';
 import { serve } from '@alisherk-editor/local-api';
-import path from 'path';
 
 const isProduction = process.env.NODE_ENV === 'production';
 
@@ -8,24 +8,24 @@ export const serveCommand = new Command()
   .command('serve [filename]')
   .description('Open a file for editing')
   .option('-p, --port <number>', 'port to run server on', '4005')
-  .action( async(filename = 'notebook.js', options: { port: string }) => {
+  .action(async (filename = 'notebook.js', options: { port: string }) => {
     try {
       const dir = path.join(process.cwd(), path.dirname(filename));
-
-      await serve(parseInt(options.port), path.basename(filename), dir, !isProduction);
-
-      console.log(`Opened ${filename}. Navigate to http://localhost:${options.port} to edit the file`);
-
-    } catch (error: any) {
-      if(error.code === 'EADDRINUSE') {
-        console.log('Port is in use. Try running on a different port')
+      await serve(
+        parseInt(options.port),
+        path.basename(filename),
+        dir,
+        !isProduction
+      );
+      console.log(
+        `Opened ${filename}. Navigate to http://localhost:${options.port} to edit the file.`
+      );
+    } catch (err: any) {
+      if (err.code === 'EADDRINUSE') {
+        console.error('Port is in use. Try running on a different port.');
       } else {
-        console.log('here the problem', error.message);
+        console.log('Heres the problem', err.message);
       }
       process.exit(1);
     }
   });
-
-//path.join(process.cwd()), path.dirname(filename); will give absolute path to where you are on comp along with dir where you are currently in;
-// will print out: /Users/alisherkabildjanov/Projects/ts-react/jbook/packages/cli/dist notes
-
--- editor/packages/cli/src/index.ts
+++ editor/packages/cli/src/index.ts
@@ -1,6 +1,7 @@
+#!/usr/bin/env node
 import { program } from 'commander';
 import { serveCommand } from './commands/serve';
 
-program.addCommand(serveCommand); 
+program.addCommand(serveCommand);
 
 program.parse(process.argv);
